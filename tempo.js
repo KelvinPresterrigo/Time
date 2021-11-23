eb1741904621a4cc79e6f967eb4ec5da38b8b657
Kelvin Presterrigo <kelvin42xd@gmail.com>
2021-11-23 15:14:58 -0300

    Cronometro

---
+++ tempo.js
@@ -0,0 +1,49 @@
+var sec=00
+var min=00
+var hora=00
+
+var interval
+
+function doisdigitos(digit){
+    if(digit<10){
+        return('0'+digit)
+    } else {
+    return(digit)
+    }
+}
+function start(){
+    pulso()
+    clearInterval(interval)
+    interval= setInterval(pulso,1000)
+    
+
+}
+
+function stop(){
+    clearInterval(interval)
+
+}
+
+function reload (){
+    clearInterval(interval)
+    sec =0 
+    min =0
+    //window.alert("tou´ve stopped at : "+document.getElementById('pulso').innerText)
+    document.getElementById('pulso').innerText="00:00:00"
+}
+
+
+function pulso(){
+     sec++
+     if(sec==60){
+         min++
+         sec= 0
+         if(min==60){
+             min=0
+             hora++
+         }
+
+     }
+     document.getElementById("pulso").innerText= doisdigitos(hora)+':'+ doisdigitos(min)+':'+ doisdigitos(sec)
+
+ }
