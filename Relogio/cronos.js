3b2de5cbdc0c70c5e0555c5a386229b6a76de580
Kelvin Presterrigo <kelvin42xd@gmail.com>
2021-12-09 14:00:54 -0300

    Cronometro

---
+++ Relogio/cronos.js
@@ -0,0 +1,50 @@
+var hour = 00
+var minutes = 00
+var second = 00
+var millisecond = 00
+
+let cron
+
+document.form_main.start.onclick = () => start();
+document.form_main.pause.onclick = () => pause();
+document.form_main.reset.onclick = () => reset();
+
+function start() {
+    pause();
+    cron = setInterval(() => { timer(); }, 10);}
+  
+  function pause() {
+    clearInterval(cron);}
+  
+  function reset() {
+    hour = 0;
+    minute = 0;
+    second = 0;
+    millisecond = 0;
+    document.getElementById('hour').innerText = '00';
+    document.getElementById('minute').innerText = '00';
+    document.getElementById('second').innerText = '00';
+    document.getElementById('millisecond').innerText = '000';
+  }
+ function timer() {
+    if ((millisecond += 10) == 1000) {
+      millisecond = 0;
+      second++;
+    }
+    if (second == 60) {
+      second = 0;
+      minute++;
+    }
+    if (minute == 60) {
+      minute = 0;
+      hour++;
+    }
+    document.getElementById('hour').innerText = returnData(hour);
+    document.getElementById('minute').innerText = returnData(minute);
+    document.getElementById('second').innerText = returnData(second);
+    document.getElementById('millisecond').innerText = returnData(millisecond);
+  }
+  
+  function returnData(input) {
+    return input > 10 ? input : `0${input}`
+  }
