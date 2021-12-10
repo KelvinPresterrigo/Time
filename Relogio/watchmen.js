24ba5be559b5c4ffd312e1b4a22cfa514e925d46
Kelvin Presterrigo <kelvin42xd@gmail.com>
2021-12-10 17:44:48 -0300

    att

--- Relogio/watchmen.js
+++ Relogio/watchmen.js
@@ -1,18 +1,19 @@
-
-const clockContainer = document.querySelector('.clock-Container')
+const clockContainer = document.querySelector(".clock-Container");
 const updateClock = () => {
-    let present = new Date()
-    let horas = present.getHours()
-    let min = present.getMinutes()
-    let seg = present.getSeconds()
-    min = zero(min)
-    seg = zero(seg)
-    function zero(x){
-        if(x < 10){
-            x = '0'+x
-        }return x
+  let present = new Date();
+  let horas = present.getHours();
+  let min = present.getMinutes();
+  let seg = present.getSeconds();
+  min = zero(min);
+  seg = zero(seg);
+  function zero(x) {
+    if (x < 10) {
+      x = "0" + x;
     }
-   const clockHTML = `${horas} : ${min} : ${seg}`
-   clockContainer.innerHTML = clockHTML
-}
-setInterval(updateClock, 1000) 
+    return x;
+  }
+  const clockHTML = `${horas} : ${min} : ${seg}`;
+  clockContainer.innerHTML = clockHTML;
+};
+setInterval(updateClock, 1000);
+ 
